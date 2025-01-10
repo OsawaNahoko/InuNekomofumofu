18a2f4f86a05e690709e9d8be9e7d92c6384e0d6
SioazinoNapoporitan <nahoko58osawa@gmail.com>
2025-01-10 23:45:36 +0900

    いぬねこもふもふのプロジェクトを微調整しました。 （何故かほぼ全てのデータが書き換えられているため、ブランチを切っています）

--- Assets/script/Character/BaseClaccs/Movement.cs
+++ Assets/script/Character/BaseClaccs/Movement.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class Movement : AnimetionSetting
 {
+    public int Chara_ID;//キャラクターによりレイヤー順序をズラす為の変数。
 
     float[] positionYArray = {2.25f,-1.6f};
     float[] positionXArray = {-6.0f,0.4f,6.0f};
 
-//ｙ軸は１か2.25上か下かの違い　ｘ軸は－６か０．４か６のうちどれか。左右真ん中の違い
+    //ｙ軸は１か2.25上か下かの違い　ｘ軸は－６か０．４か６のうちどれか。左右真ん中の違い
 
     public void OnMove()
     {
-
+        
         float x = positionXArray[Random.Range(0,positionXArray.Length)];
         float y = positionYArray[Random.Range(0,positionYArray.Length)];
 
         transform.position = new Vector3(x,y,0);
 
+        //キャラクターのレイヤー順序を変更する。
         if(y == -1.6f)
         {
-            m_Splaitrend.sortingOrder = 5;
+            m_Splaitrend.sortingOrder = 10 + Chara_ID;//下段に居たら手前のレイヤーに設定。
         }
         else if(y == 2.25f)
         {
-            m_Splaitrend.sortingOrder = 4;
+            m_Splaitrend.sortingOrder = 100 + Chara_ID;//上段に居たら奥のレイヤーに設定。
         }
 
         unSet_DownAnime();
